bec0d0ecb6e735bf0c58505d7ace89fda72e744a
szczypiorofix <Scypiur1>
2018-07-22 14:17:49 +0200

    Dodano trochę TODOsów - pomysły.

--- src/com/szczypiorofix/sweetrolls/game/enums/PlayerClass.java
+++ src/com/szczypiorofix/sweetrolls/game/enums/PlayerClass.java
@@ -2,6 +2,18 @@ package com.szczypiorofix.sweetrolls.game.enums;
 
 public enum PlayerClass {
 
+    // TODO Zrezygnować z klas postaci. Postać będzie bezklasowa i rozwijać będzie tylko umiejętności których używa (jak w TESach)
+
+    // TODO Umiejętności będą wyuczane i zwiększane u trenerów jak w Gothicu
+
+    // TODO Do każdego kafelka na mapie świata można "wejść" gdzie generowany jest losowy teren
+
+    // TODO Dany teren ma określoną ilość zasobów: rudy, drewno, żywność, roślinki które po zebraniu po pewnym czasie (losowo trochę) będzie odnawiany
+
+    // TODO Podróż na mapie głównej zajmować będzie 1 dzień na 1 kafelek (w zależności od typu terenu będize np. więcej w górach).
+    // TODO Podróż po mapach wewnętrznych zajmować będzie 10min - 1h w zależności od terenu. Mapa wewnętrzna będzie posiadała określoną wielkość np. 50 x 50.
+    // TODO Na mapach wewnętrznych będą możliwe walki, na mapie głównej - nie.
+
     WARRIOR (10, 5, 10, 5) {
         @Override
         public String getName() {
